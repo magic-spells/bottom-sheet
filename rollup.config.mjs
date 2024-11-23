de5b493c45f2a6d1400b0ffddbc83716f25e188a
CorySchulz <coryschulz@gmail.com>
2024-11-22 23:46:48 -0300

    updating config files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,58 +1,48 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import postcss from 'rollup-plugin-postcss';
-import terser from '@rollup/plugin-terser';
-import lightningcss from 'postcss-lightningcss';
-import serve from 'rollup-plugin-serve';
-import livereload from 'rollup-plugin-livereload';
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import serve from "rollup-plugin-serve";
+import copy from "rollup-plugin-copy";
 
-const isProd = process.env.NODE_ENV === 'production';
+const dev = process.env.ROLLUP_WATCH;
 
-export default {
-  input: 'src/bottom-sheet.js',
-  output: [
-    {
-      file: 'dist/bottom-sheet.min.js',
-      format: 'iife',  // ES module format for modern JavaScript environments
-      sourcemap: !isProd
+export default [
+  // Development build
+  {
+    input: "src/bottom-sheet.js",
+    output: {
+      file: "dist/bottom-sheet.js",
+      format: "iife",
+      sourcemap: dev,
     },
-    {
-      file: 'site/bottom-sheet.min.js',  // For testing or local development in the site folder
-      format: 'iife',  // UMD format for browser
-      name: 'BottomSheet',  // Global variable name in browsers
-      sourcemap: !isProd
-    }
-  ],
-  plugins: [
-    resolve(),
-    commonjs(),
-    isProd && terser({
-      mangle: {
-        keep_classnames: true,  // Preserve class names during minification
-        keep_fnames: true       // Optionally preserve function names as well
-      }
-    }),  // JS minification in production
-    postcss({
-      extensions: ['.scss', '.css'],
-      plugins: [
-        lightningcss({
-          lightningcssOptions: {
-            minify: isProd,
-            sourcemap: !isProd,
-          }
-        })
-      ],
-      extract: 'bottom-sheet.min.css',
-      minimize: false,  // Use lightningcss for minification
-      sourceMap: isProd ? false : 'inline',
-    }),
-    serve({
-      open: true,
-      contentBase: 'site', // The folder to serve files from
-      port: 3000,
-    }),
-    !isProd && livereload({
-      watch: 'site', // The folder to watch for changes
-    }),
-  ]
-};
+    plugins: [
+      resolve(),
+      dev &&
+        serve({
+          contentBase: ["dist", "demo"],
+          open: true,
+          port: 3000,
+        }),
+      copy({
+        targets: [{ src: "dist/bottom-sheet.js", dest: "demo" }],
+        hook: "writeBundle",
+      }),
+    ],
+  },
+  // Production build (minified)
+  {
+    input: "src/bottom-sheet.js",
+    output: {
+      file: "dist/bottom-sheet.min.js",
+      format: "iife",
+      sourcemap: false,
+    },
+    plugins: [
+      resolve(),
+      terser({
+        format: {
+          comments: false,
+        },
+      }),
+    ],
+  },
+];
